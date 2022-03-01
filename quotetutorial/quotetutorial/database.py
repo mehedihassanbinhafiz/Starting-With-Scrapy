285dd07956d26d18a6b00529ecd085b02ae42a60
= <mehedihassanbinhafiz@gmail.com>
2022-03-01 20:47:38 +0600

    databased is added

--- quotetutorial/quotetutorial/database.py
+++ quotetutorial/quotetutorial/database.py
@@ -2,7 +2,12 @@ import  sqlite3
 
 conn = sqlite3.connect('quote.sqlite3')
 cur = conn.cursor()
-cur.execute("""create table quotes( title text, author text, tag text """)
-
+# cur.execute("""create table quotes(
+#  title text,
+#  author text,
+#  tag text
+#   )""")
+cur.execute("""insert into quotes values ('python','mehedi','no tag')""")
 conn.commit()
 conn.close()
+
