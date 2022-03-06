0dbc361142d0871ac8954e78a4c366421a37acba
= <mehedihassanbinhafiz@gmail.com>
2022-03-06 18:43:46 +0600

    pagination added

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -2,8 +2,9 @@ import scrapy
 from ..items import QuotetutorialItem
 class QuotesSpider(scrapy.Spider): #scrapy is module Spider is a class
     name = 'quotes' # the name of the spider
+    page_number = 2
     start_urls = [
-        'https://quotes.toscrape.com/'
+        'https://quotes.toscrape.com/page/1/'
     ]
     def parse(self,response): # response contains the source code of website
 
@@ -17,7 +18,15 @@ class QuotesSpider(scrapy.Spider): #scrapy is module Spider is a class
             quote_item['title'] = quote
             quote_item['author'] = author
             quote_item['tags'] = tag
-            yield quote_item#Generator  # used insted of return
+            yield quote_item#Generator  # used insted of return. it returns value one by one
+
+        next_page = 'https://quotes.toscrape.com/page/{}/'.format(QuotesSpider.page_number)
+        if QuotesSpider.page_number < 5: #response.follow() # scrapy used it for next page
+            QuotesSpider.page_number += 1
+            yield response.follow(next_page, callback = self.parse)
+
+
+
 
 
 
